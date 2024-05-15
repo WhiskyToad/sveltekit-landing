2ab47088e48d88f30a76971f013e33276985178e
Steven <stevencraig321@gmail.com>
2024-05-15 16:27:08 +0100

    refactor: added logos

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import { enhance, type SubmitFunction } from '$app/forms';
+	import { enhance } from '$app/forms';
 	import { page } from '$app/stores';
 	import '$lib/app.css';
-	import Drawer from './Drawer.svelte';
+	import type { SubmitFunction } from '@sveltejs/kit';
+	import logo from '$lib/assets/logo.png'
 
 	const submitUpdateTheme: SubmitFunction = ({ action }) => {
 		const theme = action.searchParams.get('theme');
@@ -48,7 +49,7 @@
 <div class="bg-base-100 sticky top-0 shadow-lg z-40">
 	<div class="flex items-center justify-between p-2 lg:container lg:mx-auto">
 		<div class="">
-			<a href="/" class="font-bold text-primary text-xl">Sveltekit</a>
+			<a href="/" class="font-bold text-primary text-xl"><img src={logo} alt="Achievify" class="h-10"></a>
 		</div>
 
 		<div class="">
